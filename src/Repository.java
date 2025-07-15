c31e21671b97864ce035c328e0ed08b62387397b
ashisherikilla <134827407+ashisherikilla@users.noreply.github.com>
2025-07-15 20:21:11 +0530

    Repository.java updated

---
+++ src/Repository.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+import org.jcp.xml.dsig.internal.dom.Utils;
+
+public class Repository {
+    private Commit head;
+    private final StagingArea stagingArea = new StagingArea();
+    private final List<Commit> commitHistory = new ArrayList<>();
+
+    // init method logic
+    public void init() {
+        if (head != null) {
+            System.out.println("Repository already initialized.");
+        } else {
+            head = null;
+            commitHistory.clear();
+            System.out.println("Repository initialized.");
+        }
+    }
+
+    // add method logic
+    public void add(String fileName) {
+        stagingArea.stageFile(fileName);
+    }
+
+    // commit method logic
+    public void commit(String message) {
+        if (stagingArea.isEmpty()) {
+            System.out.println("No files to commit.");
+            return;
+        }
+        String id = Utils.generateCommitId();
+        List<FileSnapshot> snapshot = stagingArea.getSnapshot();
+        Commit newCommit = new Commit(id, message, snapshot, head);
+        head = newCommit;
+        commitHistory.add(newCommit);
+        stagingArea.clear();
+
+        System.out.println("[" + id + "] Commit created: " + message);
+    }
+
+    // log method logic
+    public void log() {
+        if (head == null) {
+            System.out.println("No Commits yet.");
+            return;
+        }
+        Commit current = head;
+        while (current != null) {
+            System.out.println("[" + current.getId() + "] " + current.getMessage());
+            current = current.getParent();
+        }
+    }
+
+    // status method logic
+    public void status(){
+        if(stagingArea.isEmpty()){
+            System.out.println("No files staged.");
+        }else{
+            System.out.println("Files staged for commit: ");
+            for(String file : stagingArea.getStagedFiles()){
+                System.out.println(" - "+file);
+            }
+        }
+    }
+}
